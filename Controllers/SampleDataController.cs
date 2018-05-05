5807e02c6756c0ca3c8b2de4cf34936af649bff7
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-05 22:09:31 +0100

    rename project, solution files, namespace

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace test.Controllers
+namespace App.Web.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
