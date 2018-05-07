04263217509cb0d3310d75bc21dfe7f13ff7677e
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-07 01:53:54 +0100

    Remove vuex for now

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
 import router from './router'
-import store from './store'
-import { sync } from 'vuex-router-sync'
 import App from 'components/app-root'
 import './stylus/main.styl';
 import Vuetify from 'vuetify';
@@ -10,16 +8,12 @@ import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 Vue.prototype.$http = axios;
 
-sync(store, router)
-
 const app = new Vue({
-    store,
     router,
     ...App
 })
 
 export {
     app,
-    router,
-    store
+    router
 }
