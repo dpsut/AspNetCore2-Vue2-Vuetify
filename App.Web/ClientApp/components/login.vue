8b83033eeac634f6a8937e66b6765c895a921d23
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-10 23:07:25 +0100

    style login form

--- App.Web/ClientApp/components/login.vue
+++ App.Web/ClientApp/components/login.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <v-btn @click="login">Login</v-btn>
-    </div>
+    <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4 class="mt-5">
+            <v-card class="elevation-12">
+                <v-toolbar dark color="primary">
+                    <v-toolbar-title>Login</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field prepend-icon="email" name="login" label="Login" type="text"></v-text-field>
+                        <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="login" color="primary">Login</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
