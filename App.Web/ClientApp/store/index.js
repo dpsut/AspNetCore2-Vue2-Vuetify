d86c8ac520b209ada18110855f8e4ec78a7022a9
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-11 01:51:50 +0100

    only persist the authkey to session storage

--- App.Web/ClientApp/store/index.js
+++ App.Web/ClientApp/store/index.js
@@ -30,5 +30,5 @@ export default new Vuex.Store({
     state,
     mutations,
     actions,
-    plugins: [createPersistedState({ storage: window.sessionStorage })]
+    plugins: [createPersistedState({ storage: window.sessionStorage, paths: ['authKey'] })]
 });
