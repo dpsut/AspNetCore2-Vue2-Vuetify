6818ef9f2b7b4c3aa274cc1732cba18bc289dcd6
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-13 15:49:52 +0100

    add a stepper to home page where we can show some vuetify form elements

--- App.Web/ClientApp/components/home-page.vue
+++ App.Web/ClientApp/components/home-page.vue
@@ -1,45 +1,51 @@
 <template>
-    <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-            <li><a href="https://get.asp.net/">ASP.NET Core</a> and <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">C#</a>                    for cross-platform server-side code</li>
-            <li><a href="https://vuejs.org/">Vue.js</a> for client-side code</li>
-            <li><a href="https://webpack.js.org/">Webpack</a> for building and bundling client-side resources</li>
-            <li><a href="http://getbootstrap.com/">Bootstrap</a> for layout and styling</li>
-            <li><a href="api/SampleData/WeatherForecasts">API sample data</a> from the dotnet controller</li>
-        </ul>
-        <p>To help you get started, we've also set up:</p>
-        <ul>
-            <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return
-                here.</li>
-            <li><strong>Webpack dev middleware</strong>. In development mode, there's no need to run the <code>webpack</code>                    build tool. Your client-side resources are dynamically built on demand. Updates are available as soon
-                as you modify any file.</li>
-            <li><strong>Hot module replacement</strong>. In development mode, you don't even need to reload the page after
-                making most changes. Within seconds of saving changes to files, your Vue.js app will be rebuilt and
-                a new instance injected is into the page.</li>
-            <li><strong>Code splitting and lazy loading</strong>. Vue.js components may optionally be bundled individually and
-                loaded on demand. For example, the code and template for 'Counter' is not loaded until you navigate to
-                it..</li>
-            <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled,
-                and the <code>webpack</code> build tool produces minified static CSS and JavaScript files.</li>
-        </ul>
-
-        <br><br>
-
-        <h2>This Template brought to you by <a href="http://DevHelp.Online">DevHelp.Online</a></h2>
-        <strong>Consulting | Development | Training | Workshops</strong><br>
-        <p>Get your Team or Application up to speed by working with some of the leading industry experts in JavaScript & ASP.NET!</p>
-
-        <strong>Contact us today: </strong>
-        <p><a href="mailto:hello@devhelp.online">Hello@DevHelp.Online</a></p>
-    </div>
+    <v-stepper v-model="e1">
+        <v-stepper-header>
+            <v-stepper-step :complete="e1 > 1" step="1">User Agreement</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="e1 > 2" step="2">Company</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="e1 > 3" step="3">Goals</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="e1 > 4" step="4">Training</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="5">Next Steps</v-stepper-step>
+        </v-stepper-header>
+        <v-stepper-items>
+            <v-stepper-content step="1">
+                <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+                <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="2">
+                <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+                <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="3">
+                <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+                <v-btn color="primary" @click.native="e1 = 4">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="4">
+                <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+                <v-btn color="primary" @click.native="e1 = 5">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="5">
+                <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+                <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
+                <v-btn flat>Cancel</v-btn>
+            </v-stepper-content>
+        </v-stepper-items>
+    </v-stepper>
 </template>
 
 <script>
 export default {
     data() {
         return {
+            e1: 0
         }
     },
 }
