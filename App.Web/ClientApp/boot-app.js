d1a6e1bb6520ead5abdea595a2eead7ee23986ce
David Sutcliffe <davidsutcliffe@mail.com>
2018-05-13 11:33:32 +0100

    use base64 encoded material design icons (fix IE11 font loading issue)

--- App.Web/ClientApp/boot-app.js
+++ App.Web/ClientApp/boot-app.js
@@ -1,6 +1,6 @@
 import "babel-polyfill"
 import "./static/css/site.css"
-import "material-design-icons-iconfont/dist/material-design-icons.css"
+import "material-icons-base64/index.css"
 import 'axios-progress-bar/dist/nprogress.css'
 require('./static/favicon.ico');
 
